3fc42f000ab34f7fba685b76761626e04df0d516
priyanshuapril2004 <priyanshubhagat30@gmail.com>
2026-01-20 11:06:48 +0530

    operators assignment

---
+++ operators.py
@@ -0,0 +1,35 @@
+# x1=int(input("enter the subject1" ))
+# x2=int(input("enter the subject2" ))
+# x3=int(input("enter the subject3" ))
+# x4=int(input("enter the subject4" ))
+# x5=int(input("enter the subject5" ))
+# sum=(x1+x2+x3+x4+x5)
+# p=(( sum/500)*100)  
+# print(p)
+
+# a=int(input("Enter the no.1" ))
+# b=int(input("Enter the no.2" ))
+# c=int(input("Enter the no.3" ))
+# sum=(a+b+c)
+# print(sum) 
+
+# r=int(input("Enter the no."))
+# print(r*r)
+
+# c=float(input("Enter temperature in celsius" ))
+# f=(c*9/5)+32
+# print(str(c))
+# print(str(f))
+
+# a=int(input("enter the first number" ))
+# b=int(input("enter the second number" ))
+# quoitent=a//b
+# remainder=a%b
+# print("Quoitent is =",quoitent)
+# print("Remainder is =",remainder)
+
+# P=int(input("enter the profit = "))
+# R=int(input("enter the rate = "))
+# T=int(input("enter the time = "))
+# SI=((P*R*T)/100)
+# print(SI)
