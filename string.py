bd4b39bdb23881484f3eae0ec06ca9547b68114c
priyanshuapril2004 <priyanshubhagat30@gmail.com>
2026-01-22 12:49:22 +0530

    update

---
+++ string.py
@@ -0,0 +1,24 @@
+# str=(input("enter the string = "))
+# if len(str)<2:
+#     print("not valid" )
+# else:
+#     result=(str[:2]+str[-2:])    
+#     print(result)
+
+# str1=(input("enter the sring1 = "))
+# str2=(input("enter the string2 = "))
+
+# new1=str2[0]+str1[1:]
+# new2=str1[0]+str2[1:]
+# print(new1,new2)
+
+# str=(input("enter the string"))
+# if len(str)>3:
+#     print(str+"ing")
+# else :
+#     print(str+"ly")
+        
+# str=(input("enter the string =" ))
+# n=int(input(" enter the index to remove ="))
+# new_str=str[:n]+str[n+1:]
+# print("after remove character =",new_str )
