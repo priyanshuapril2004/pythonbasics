95e638d55cd6cb6c597f55c8f17dafaf83d37377
priyanshuapril2004 <priyanshubhagat30@gmail.com>
2026-01-25 13:25:25 +0530

    update

---
+++ list.py
@@ -0,0 +1,64 @@
+# l1=["raju","monu","sonu","ram","riya"]
+# print(l1)
+# new_friend=input(("enter the new friend = "))
+# l1.append(new_friend)
+# print(l1)
+# imp_friend=(input("enter the important friend = "))
+# position=int(input("enter the position = "))
+# if position<0 or position>len(l1):
+#     print("invalid position ")
+# else:
+#     print(l1.insert(position,imp_friend))
+# print(l1)    
+
+
+# l2=[1,2,3,4,5,6,7,8,9,10]
+# print("first ten numbers =",l2)
+
+
+# l3=[1,10,100,3,6,8]
+# l3.insert(3,59)
+# l3.append(5)
+# print(l3)
+# print("lenth of list = ",len(l3))
+
+# l4=["hi","hello","sir","work","is","difficult"]
+# print(l4)
+# short_words=[]
+# for word in l4:
+#   if len(word)<4:
+#       short_words.append(word)
+# print("word less than 4 words = ",short_words)
+
+
+# l5=[]
+# for i in range(20):
+#     if i%2==0:
+#         print('even',end=" , ")
+#     else:
+#         print('odd',end=" , ") 
+        
+        
+        
+        
+# l6=[]
+# for i in range(1,1000):
+#     if i%7==0:
+#         print(i,end=" ")
+#     l6.append(i)
+            
+            
+# l7= ['hell o','he  i','man si','me hak','Arsh']
+
+# l7= [i.count(' ') for i in l1 if ' ' in i]
+# print(l7)
+# print(sum(l7))
+
+
+# l8=[1,2,3,4] 
+# l9=[2,3,4,5]           
+# common=[]
+# for i in l8:
+#     if i in l9 and i not in common:
+#         common.append(i)
+# print(common)         
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,5 @@
-if (a is b):
-    print("a and b are equal")
-else:
-    print("a and b are not equal")
+l7= ['hell o','he  i','man si','me hak','Arsh']
+
+# l7= [i.count(' ') for i in l1 if ' ' in i]
+# print(l7)
+# print(sum(l7))
